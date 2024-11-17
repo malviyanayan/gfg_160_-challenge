c2180ab6fa64ef2aa109cb957c052b7af2dad606
nayan malviya <naymalviya@gmail.com>
2024-11-17 23:12:02 +0530

    Day-3

--- gfg/Solution.java
+++ gfg/Solution.java
@@ -21,15 +21,38 @@ public class Solution{
             }
         }
     }
+
+
+    // ########## Day-2 ##########
+    // ||||||||| Completed ********
+    public void reverseArray(int arr[]) {
+        int s = 0,e=arr.length-1;
+        while(s<e){
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+
+            s++;
+            e--;
+        }
+    }
     
     public static void main(String[] args) {
         Solution son = new Solution(); // Requered for run the program
         // int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
         // int[] arr = {10, 20, 30};
         // int[] arr = {0, 0};
-        int[] arr = {3, 5, 0, 0, 4};
-        son.pushZerosToEnd(arr);
+        // int[] arr = {3, 5, 0, 0, 4};
+        // son.pushZerosToEnd(arr);
+        
+        // day-3
+        // int[] arr = {1, 4, 3, 2, 6, 5};
+        // int[] arr = {4, 5, 2};
+        int[] arr = {1};
+        son.reverseArray(arr);
 
-        for(int next : arr)System.out.println(next);
+
+
+        for(int next : arr)System.out.print(next+", ");
     }
 }
