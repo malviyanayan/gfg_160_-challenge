953234dc7b06a9872372032fdd2d3c4db7a713b5
nayan malviya <naymalviya@gmail.com>
2024-11-18 19:01:58 +0530

    day-3

--- gfg/Solution.java
+++ gfg/Solution.java
@@ -37,6 +37,27 @@ public class Solution{
         }
     }
     
+
+
+    // ########## Day-2 ##########
+    // ||||||||| Completed ********
+    void rotateArr(int arr[], int d) {
+        if(arr.length == 0 || (d = d % arr.length)==0)return;
+        int[] dup = new int[d];
+        int i;
+        for(i = 0;i<d;i++){
+            dup[i] = arr[i];
+        }
+        
+        int j = 0;
+        while(i != arr.length){
+            arr[j++] = arr[i];
+            i++;
+        }
+        i=0;
+        while(j != arr.length)arr[j++] = dup[i++];
+
+    }
     public static void main(String[] args) {
         Solution son = new Solution(); // Requered for run the program
         // int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
@@ -48,9 +69,19 @@ public class Solution{
         // day-3
         // int[] arr = {1, 4, 3, 2, 6, 5};
         // int[] arr = {4, 5, 2};
-        int[] arr = {1};
-        son.reverseArray(arr);
+        // int[] arr = {1};
+        // son.reverseArray(arr);
 
+        //day-4
+        // int[] arr = {1, 2, 3, 4, 5};
+        // int d = 2;
+        // int[] arr = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        // int d = 3;
+        // int[] arr = {7, 3, 9, 1};
+        // int d = 9;
+        int[] arr = {};
+        int d = 9;
+        son.rotateArr(arr, d);
 
 
         for(int next : arr)System.out.print(next+", ");
